374215ed1977f522b8d21b317cf6fde44aa8e62f
Ari Porad <ari@porad.com>
2015-08-17 15:48:55 -0700

    feat(last-release-npm): initial release
    Moved the code out of core into it's own project.
    Previously: https://github.com/semantic-release/semantic-release/blob/77349fcdf193467967fa4a85daf2ee3fa812779b/src/lib/last-release.js
    
    Refs semantic-release/semantic-release#56, semantic-release/semantic-release#51

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# last-release-npm
+# semantic-release last-release-npm
+[![npm version](https://badge.fury.io/js/%40semantic-release%2Flast-release-npm.svg)](http://badge.fury.io/js/%40semantic-release%2Flast-release-npm)
+[![Build Status](https://travis-ci.org/semantic-release/last-release-npm.svg?branch=master)](https://travis-ci.org/semantic-release/last-release-npm)
+[![Coverage Status](https://coveralls.io/repos/semantic-release/last-release-npm/badge.svg)](https://coveralls.io/r/semantic-release/last-release-npm)
+[![Dependency Status](https://david-dm.org/semantic-release/last-release-npm.svg)](https://david-dm.org/semantic-release/last-release-npm)
+[![devDependency Status](https://david-dm.org/semantic-release/last-release-npm/dev-status.svg)](https://david-dm.org/semantic-release/last-release-npm#info=devDependencies)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
---
+++ test/specs/index.js
@@ -0,0 +1,76 @@
+const { defaults } = require('lodash')
+const test = require('tap').test
+
+require('../mocks/registry')
+const lastRelease = require('../../')
+
+const npm = {
+  registry: 'http://registry.npmjs.org/',
+  tag: 'latest'
+}
+
+test('last release from registry', (t) => {
+  t.plan(5)
+
+  t.test('get release from package name', (tt) => {
+    lastRelease({}, {
+      pkg: {name: 'available'},
+      npm
+    }, (err, release) => {
+      tt.error(err)
+      tt.is(release.version, '1.33.7', 'version')
+      tt.is(release.gitHead, 'HEAD', 'gitHead')
+
+      tt.end()
+    })
+  })
+
+  t.test('get release from a tagged package\'s name', (tt) => {
+    lastRelease({}, {
+      pkg: {name: 'tagged'},
+      npm: defaults({tag: 'foo'}, npm)
+    }, (err, release) => {
+      tt.error(err)
+      tt.is(release.version, '0.8.15', 'version')
+      tt.is(release.gitHead, 'bar', 'gitHead')
+
+      tt.end()
+    })
+  })
+
+  t.test('get error from an untagged package\'s name', (tt) => {
+    lastRelease({}, {
+      pkg: {name: 'untagged'},
+      npm: defaults({tag: 'bar'}, npm)
+    }, (err) => {
+      tt.is(err.code, 'ENODISTTAG', 'error')
+
+      tt.end()
+    })
+  })
+
+  t.test('get release from scoped package name', (tt) => {
+    lastRelease({}, {
+      pkg: {name: '@scoped/available'},
+      npm
+    }, (err, release) => {
+      tt.error(err)
+      tt.is(release.version, '1.33.7', 'version')
+      tt.is(release.gitHead, 'HEAD', 'gitHead')
+
+      tt.end()
+    })
+  })
+
+  t.test('get nothing from not yet published package name', (tt) => {
+    lastRelease({}, {
+      pkg: {name: 'unavailable'},
+      npm
+    }, (err, release) => {
+      tt.error(err)
+      tt.is(release.version, undefined, 'no version')
+
+      tt.end()
+    })
+  })
+})
