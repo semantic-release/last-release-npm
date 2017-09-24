bb676d0a7e5dffa7eac18d991e271e83ecf698b8
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-09-24 18:48:28 -0400

    test: Use ava, nyc and codecov
    Refactor the test to be independent from each others

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [![npm version](https://badge.fury.io/js/%40semantic-release%2Flast-release-npm.svg)](http://badge.fury.io/js/%40semantic-release%2Flast-release-npm)
 [![Build Status](https://travis-ci.org/semantic-release/last-release-npm.svg?branch=next)](https://travis-ci.org/semantic-release/last-release-npm)
-[![Coverage Status](https://coveralls.io/repos/semantic-release/last-release-npm/badge.svg?branch=next&service=github)](https://coveralls.io/github/semantic-release/last-release-npm?branch=next)
 
 [![Dependency Status](https://david-dm.org/semantic-release/last-release-npm/next.svg)](https://david-dm.org/semantic-release/last-release-npm/next)
 [![devDependency Status](https://david-dm.org/semantic-release/last-release-npm/next/dev-status.svg)](https://david-dm.org/semantic-release/last-release-npm/next#info=devDependencies)
@@ -12,3 +11,4 @@
 
 [![Join the chat at https://gitter.im/semantic-release/semantic-release](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/semantic-release/semantic-release?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/last-release-npm)
+[![Codecov](https://img.shields.io/codecov/c/github/semantic-release/last-release-npm.svg)](https://codecov.io/gh/semantic-release/last-release-npm)
---
+++ test/index.test.js
@@ -0,0 +1,110 @@
+import test from 'ava';
+import nock from 'nock';
+import {promisify} from 'util';
+import {defaults} from 'lodash';
+import SemanticReleaseError from '@semantic-release/error';
+import lastRelease from '../index';
+import {npm, mock, available, unpublished} from './helpers/mock-registry';
+
+test.afterEach.always(t => {
+  nock.cleanAll();
+});
+
+test.serial('Get release from package name', async t => {
+  const name = 'available';
+  const registry = available(name);
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm});
+
+  t.is(release.version, '1.33.7');
+  t.is(release.gitHead, 'HEAD');
+  t.is(release.tag, 'latest');
+  t.true(registry.isDone());
+});
+
+test.serial("Get release from a tagged package's name", async t => {
+  const name = 'tagged';
+  const registry = available(name);
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm: defaults({tag: 'foo'}, npm)});
+
+  t.is(release.version, '0.8.15');
+  t.is(release.gitHead, 'bar');
+  t.is(release.tag, 'foo');
+  t.true(registry.isDone());
+});
+
+test.serial('Get release from a fallbackTag', async t => {
+  const name = 'tagged';
+  const registry = available(name);
+  const release = await promisify(lastRelease)(
+    {},
+    {pkg: {name}, options: {fallbackTags: {bar: 'latest'}}, npm: defaults({tag: 'bar'}, npm)}
+  );
+
+  t.is(release.version, '1.33.7');
+  t.is(release.gitHead, 'HEAD');
+  t.is(release.tag, 'bar');
+  t.true(registry.isDone());
+});
+
+test.serial("Get error from an untagged package's name", async t => {
+  const name = 'untagged';
+  const registry = available(name);
+  const error = await t.throws(
+    promisify(lastRelease)({}, {pkg: {name}, npm: defaults({tag: 'bar'}, npm)}),
+    /There is no release with the dist-tag "bar" yet/
+  );
+
+  t.true(error instanceof SemanticReleaseError);
+  t.is(error.code, 'ENODISTTAG');
+  t.true(registry.isDone());
+});
+
+test.serial('Get release from scoped package name', async t => {
+  const name = '@scoped/available';
+  const registry = available(name);
+
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm});
+  t.is(release.version, '1.33.7');
+  t.is(release.gitHead, 'HEAD');
+  t.is(release.tag, 'latest');
+  t.true(registry.isDone());
+});
+
+test.serial('Get nothing from completely unpublished package name', async t => {
+  const name = 'completely-unpublished';
+  const registry = unpublished(name);
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm});
+
+  t.is(release.version, undefined);
+  t.true(registry.isDone());
+});
+
+test.serial('Get nothing from not yet published package name (unavailable)', async t => {
+  const name = 'unavailable';
+  const registry = mock(name).reply(404, {});
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm});
+
+  t.is(release.version, undefined);
+  t.true(registry.isDone());
+});
+
+test.serial('Get nothing from not yet published package name (unavailable w/o response body)', async t => {
+  const name = 'unavailable-no-body';
+  const registry = mock(name).reply(404);
+  const release = await promisify(lastRelease)({}, {pkg: {name}, npm});
+
+  t.is(release.version, undefined);
+  t.true(registry.isDone());
+});
+
+test.serial('Get nothing from not yet published package name (unavailable w/o status code)', async t => {
+  const name = 'unavailable-no-404';
+  const registry = mock(name).replyWithError({message: 'not found', statusCode: 500, code: 'E500'});
+  const release = await promisify(lastRelease)(
+    {retry: {count: 1, factor: 1, minTimeout: 1, maxTimeout: 2}},
+    {pkg: {name: 'unavailable-no-404'}, npm}
+  );
+
+  t.is(release.version, undefined);
+  t.true(registry.isDone());
+});
